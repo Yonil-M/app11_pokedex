64527a5b4e32647cb00f1dbba5f8d6d2b27e8677
Yonil <201913301c@utea.edu.pe>
2022-11-28 13:55:15 -0500

    commit  video16

---
+++ android/app/src/main/kotlin/com/example/app11_pokedex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app11_pokedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:app11_pokedex/pages/home_page.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "PokedexApp",
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
