48d1ca9fde338d5225f4b00fc5bb6aeb649b8a41
Yonil <201913301c@utea.edu.pe>
2022-11-29 18:45:59 -0500

    commit  video16

---
+++ lib/ui/widgets/iten_pokemon.dart
@@ -0,0 +1,58 @@
+import 'package:app11_pokedex/ui/widgets/item_type.dart';
+import 'package:flutter/material.dart';
+
+class ItemPokemonWidget extends StatelessWidget {
+ String name;
+ String image;
+ List<String> types;
+
+ ItemPokemonWidget({
+  required this.name,
+  required this.image,
+  required this.types,
+ });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xff4ccfb2),
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom:-20 ,
+                        right: -20,
+                        child: Image.asset('assets/images/pokeball.png',
+                        height: 120.0,
+                        color: Colors.white.withOpacity(0.27),),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20.0,horizontal: 12.0),
+                        child: Column(children: [
+                          Text(name,style: TextStyle(
+                            color: Colors.white,
+                            fontSize:16.0 ,
+                            fontWeight: FontWeight.bold,
+                          ),),
+
+                         //Column(children: types.map((e) => ItemTypeWidget()).toList()
+                         //,),
+                        ...types.map((e) => ItemTypeWidget(text: e,)).toList(),
+
+
+                        ],),
+                      ),
+
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Image.network(image)),
+                    ],
+                  ),
+                );
+  }
+}
